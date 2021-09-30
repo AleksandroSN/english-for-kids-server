157c83c1ac97c17aeb1a954e28756c80bd0af7d2
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-30 20:12:53 +0300

    20:12

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,19 +1,19 @@
   
-#!/bin/bash
+# !/bin/bash
 
-#give permission for everything in the express-app directory
+# give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ubuntu/EFKServer/english-for-kids-server
 
-#navigate into our working directory where we have all our github files
+# navigate into our working directory where we have all our github files
 cd /home/ubuntu/EFKServer/english-for-kids-server
 
-#add npm and node to path
+# add npm and node to path
 # export NVM_DIR="$HOME/.nvm"	
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
+# install node modules
 npm install
 
-#start our node app in the background
+# start our node app in the background
 pm2 start all --watch
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# !/bin/bash
 
-#download node and npm
+# download node and npm
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 # . ~/.nvm/nvm.sh
 # nvm install node
 
-#create our working directory if it doesnt exist
+# create our working directory if it doesnt exist
  DIR="/home/ubuntu/EFKServer/english-for-kids-server"
  if [ -d "$DIR" ]; then
    sudo rm -rf ${DIR}
