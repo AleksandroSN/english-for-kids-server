d4a02c140e597baa99fac6a8e78966764ae63c75
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2021-09-30 19:21:56 +0300

    test 19:21

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,12 +5,12 @@
 sudo chmod -R 777 EFKServer/english-for-kids-server
 
 #navigate into our working directory where we have all our github files
-cd EFKServer/english-for-kids-server
+cd /home/ubuntu/EFKServer/english-for-kids-server
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install node
 
 #create our working directory if it doesnt exist
 # DIR="EFKServer/english-for-kids-server"
@@ -13,6 +13,6 @@ nvm install node
 #   echo "Creating ${DIR} directory"
 #   mkdir ${DIR}
 # fi
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
-fi
+# if [ -f .env ]; then
+#   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+# fi
